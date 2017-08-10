7e7a85a3e3f90dc162321831901055f04a3bab53
Brad McDavid <brad@wsol.com>
2017-08-10 15:42:43 -0400

    added net40 test for DotNetStarter.Web. fixed net45 test dll name.

---
+++ tests/DotNetStarter.Web.Tests.Net40/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+using System.Reflection;
+
+[assembly: AssemblyTitle("DotNetStarter.Web.Tests.Net45")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DotNetStarter.Web.Tests.Net45")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
